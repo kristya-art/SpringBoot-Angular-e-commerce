a0ee2d315b9219921f59a4559e368fa4c5911d71
kristya <shiriagina@yahoo.com>
2020-08-19 01:03:09 +0200

    created new spring-boot project and added needed dependecies

---
+++ spring-backend/GiftShop/src/test/java/com/example/GiftShop/GiftShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GiftShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
